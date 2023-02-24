fd7e24990f2526ad81ec380f73f5d11ed94ca3bf
azikar24 <44696094+azikar24@users.noreply.github.com>
2023-02-24 18:33:53 +0300

    enhancements

--- app/src/main/java/com/azikar24/wormaceptor/App.kt
+++ app/src/main/java/com/azikar24/wormaceptor/App.kt
@@ -5,27 +5,12 @@
 package com.azikar24.wormaceptor
 
 import android.app.Application
-import android.os.StrictMode
 import com.azikar24.wormaceptor.persistence.WormaCeptorPersistence
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        StrictMode.setThreadPolicy(
-            StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .penaltyLog()
-                .build()
-        )
-        StrictMode.setVmPolicy(
-            StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .build()
-        )
         WormaCeptor.storage = WormaCeptorPersistence.getInstance(this)
 //        WormaCeptor.storage = WormaCeptorIMDB.getInstance()
         WormaCeptor.addAppShortcut(this)

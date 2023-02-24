5f52c2725e1fc507b7b82c37185ea2a8f08ecda5
azikar24 <44696094+azikar24@users.noreply.github.com>
2023-02-24 13:28:52 +0300

    enhancements

--- WormaCeptor-no-op/src/main/java/com/azikar24/wormaceptor/WormaCeptor.kt
+++ WormaCeptor-no-op/src/main/java/com/azikar24/wormaceptor/WormaCeptor.kt
@@ -4,22 +4,18 @@
 
 package com.azikar24.wormaceptor
 
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import com.azikar24.wormaceptor.internal.data.WormaCeptorStorage
 
 object WormaCeptor {
     var storage: WormaCeptorStorage? = null
 
-    fun getLaunchIntent(context: Context?): Intent? {
-        return null
-    }
+    fun getLaunchIntent(context: Context?): Intent? = null
 
     fun logUnexpectedCrashes() = Unit
-    @TargetApi(Build.VERSION_CODES.N_MR1)
+
     fun addAppShortcut(context: Context?): String? = null
 
-    fun startActivityOnShake(context: Context?) {}
+    fun startActivityOnShake(context: Context?) = Unit
 }
